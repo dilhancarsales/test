25969d05a25330d654f23fe5b3210b04bd13d830
dilhancarsales <dilhan.jayathilake@carsales.com.au>
2020-10-07 19:45:10 +1100

    sss

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ var gm = require('gm');
 
 console.log(' bababa! ');
 
+/*
 gm('files/test.jpg')
 .resize(353, 257)
 .autoOrient()
+//.adjoin()
+
 .write('out/test.jpg', function (err) {
   if (!err) console.log(' hooray! ');
 
+  console.log(err);
+});*/
+
+gm('files/sample_pdf.pdf')
+.adjoin()
+.write('out/sample_pdf-%0d.jpg', function(err){
   console.log(err);
 });
